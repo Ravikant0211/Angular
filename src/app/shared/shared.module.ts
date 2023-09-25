850d20ecd8dbb4cba5c27fd5587d7f9c4ae3a056
Ravi kant <Ravikant02111@gmail.com>
2023-09-25 19:46:32 +0530

    fix: basic architecture and header

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+
+import { HeaderComponent } from './header/header.component';
+import { SharedRoutingModule } from './shared-routing.module';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule, SharedRoutingModule, MatToolbarModule, MatButtonModule],
+  exports: [HeaderComponent],
+})
+export class SharedModule {}
