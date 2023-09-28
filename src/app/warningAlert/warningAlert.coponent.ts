45f141e99ed4a3e7b51a612ef7b603249df8bbba
Ravi kant <Ravikant02111@gmail.com>
2023-09-28 17:51:20 +0530

    done: first angular assignment

---
+++ src/app/warningAlert/warningAlert.coponent.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warningAlert.component.html',
+    styles: [`
+        h3 {
+            background: red;
+            color: white;
+        }
+    `]
+})
+export class WarningAlertComponent {
+
+}
