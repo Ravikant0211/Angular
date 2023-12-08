62cfc1033fbe099a9f77399a72c1151812bfbf69
Ravi kant <Ravikant02111@gmail.com>
2023-12-08 22:26:26 +0530

    filled components with some life

--- recipeapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipeapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEn4_g5IzU7ek2RHIvV4CdW0x5fE5_HzmkBw&usqp=CAU'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEn4_g5IzU7ek2RHIvV4CdW0x5fE5_HzmkBw&usqp=CAU')
+  ];
 
+  constructor() {}
 }
