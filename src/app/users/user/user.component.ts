10c7a392ac9e791815c5a7c7b7c56819b6c538f2
Ravi kant <Ravikant02111@gmail.com>
2023-12-20 23:04:39 +0530

    first commit

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit, OnDestroy {
+  user: {id: number, name: string};
+  paramSubscription: Subscription;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // console.log('ngOnInit runs!!');
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name'],
+    }
+    this.paramSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
+  }
+
+  // You don't need to clear subscription here
+  // because in case of params subscription angular
+  // itself unsubscribe to observables when component
+  // is destroyed.
+  ngOnDestroy(): void {
+    this.paramSubscription.unsubscribe();
+  }
+}
