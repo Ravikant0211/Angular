96ec1e3aa8d60dee9528a994a7a2ddb70fa08a96
Ravi kant <Ravikant02111@gmail.com>
2023-12-25 20:20:53 +0530

    added: routing

--- recipeapp/src/app/recipes/recipes.component.ts
+++ recipeapp/src/app/recipes/recipes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipes.model';
+import { Component } from '@angular/core';
+
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -8,17 +8,6 @@ import { RecipeService } from './recipe.service';
   styleUrls: ['./recipes.component.css'],
   providers: [RecipeService]
 })
-export class RecipesComponent implements OnInit{
-  recipeSelected: Recipe;
-
-  constructor(private recipeService: RecipeService) {}
-
-  ngOnInit(): void {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.recipeSelected = recipe;
-        }
-      );
-  }
+export class RecipesComponent{
+  constructor() {}
 }
